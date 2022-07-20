aa353352a20bfdc9c3124dd906c1c52bc2cc6333
kisah <data.kisahtegar@gmail.com>
2022-07-20 21:27:52 +0700

    User Interfaces

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../../../../core/error/failures.dart';
 import '../../../../core/usecases/usecase.dart';
@@ -29,43 +30,70 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     required this.getRandomNumberTrivia,
     required this.inputConverter,
   }) : super(Empty()) {
-    on<GetTriviaForConcreteNumber>((event, emit) {
+    on<GetTriviaForConcreteNumber>((event, emit) async {
+      debugPrint('[on<GetTriviaForConcreteNumber>]: im inside Concrete');
       final inputEither =
           inputConverter.stringToUnsignedInteger(event.numberString);
-      inputEither.fold((failure) {
-        emit(const Error(message: INVALID_INPUT_FAILURE_MESSAGE));
-      }, (integer) async {
-        emit(Loading());
-        final failureOrTrivia =
-            await getConcreteNumberTrivia(Params(number: integer));
-        _eitherLoadedOrErrorState(failureOrTrivia!);
-      });
+      debugPrint('[on<GetTriviaForConcreteNumber>]: test');
+      await inputEither.fold(
+        (failure) async {
+          debugPrint('[on<GetTriviaForConcreteNumber>]: test');
+          emit(Error(message: INVALID_INPUT_FAILURE_MESSAGE));
+        },
+        (integer) async {
+          debugPrint(
+              '[on<GetTriviaForConcreteNumber>]: Inside inputEither.fold(integer)');
+          emit(Loading());
+          final failureOrTrivia =
+              await getConcreteNumberTrivia(Params(number: integer));
+          failureOrTrivia!.fold(
+            (failure) {
+              debugPrint(
+                  '[on<GetTriviaForConcreteNumber>]: Inside failureOrTrivia.fold(failure)');
+              emit(Error(message: _mapFailureToMessage(failure)));
+            },
+            (trivia) {
+              debugPrint(
+                  '[on<GetTriviaForConcreteNumber>]: Inside failureOrTrivia.fold(trivia)');
+              debugPrint(trivia.toString());
+              // await emit.forEach();
+              emit(Loaded(trivia: trivia));
+            },
+          );
+        },
+      );
     });
-
     on<GetTriviaForRandomNumber>((event, emit) async {
+      debugPrint('[on<GetTriviaForRandomNumber>]: im inside');
       emit(Loading());
       final failureOrTrivia = await getRandomNumberTrivia(NoParams());
-      _eitherLoadedOrErrorState(failureOrTrivia!);
+      failureOrTrivia!.fold((failure) {
+        debugPrint('[on<GetTriviaForRandomNumber>]: Im passing in failure');
+        emit(Error(message: _mapFailureToMessage(failure)));
+      }, (trivia) {
+        debugPrint('[on<GetTriviaForRandomNumber>]: Im passing Loaded');
+        debugPrint(trivia.toString());
+        emit(Loaded(trivia: trivia));
+      });
     });
   }
 
-  Stream<NumberTriviaState> _eitherLoadedOrErrorState(
-      Either<Failure, NumberTrivia> failureOrTrivia) async* {
-    failureOrTrivia.fold((failure) {
-      emit(Error(message: _mapFailureToMessage(failure)));
-    }, (trivia) {
-      emit(Loaded(trivia: trivia));
-    });
-  }
+  // Stream<NumberTriviaState> _eitherLoadedOrErrorState(
+  //     Either<Failure, NumberTrivia> failureOrTrivia) async* {
+  //   failureOrTrivia.fold((failure) {
+  //     emit(Error(message: _mapFailureToMessage(failure)));
+  //   }, (trivia) {
+  //     emit(Loaded(trivia: trivia));
+  //   });
+}
 
-  String _mapFailureToMessage(Failure failure) {
-    switch (failure.runtimeType) {
-      case ServerFailure:
-        return SERVER_FAILURE_MESSAGE;
-      case CacheFailure:
-        return CACHE_FAILURE_MESSAGE;
-      default:
-        return 'Unexpected error';
-    }
+String _mapFailureToMessage(Failure failure) {
+  switch (failure.runtimeType) {
+    case ServerFailure:
+      return SERVER_FAILURE_MESSAGE;
+    case CacheFailure:
+      return CACHE_FAILURE_MESSAGE;
+    default:
+      return 'Unexpected error';
   }
 }
---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/number_trivia_bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TriviaControls> createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  late String inputStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // TextField
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputStr = value;
+          },
+          onSubmitted: (_) {
+            addConcrete();
+          },
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: ElevatedButton(
+                onPressed: addConcrete,
+                child: const Text('Search'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: addRandom,
+                child: const Text('Get random trivia'),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  void addConcrete() {
+    controller.clear();
+    context.read<NumberTriviaBloc>().add(GetTriviaForConcreteNumber(inputStr));
+  }
+
+  void addRandom() {
+    controller.clear();
+    context.read<NumberTriviaBloc>().add(GetTriviaForRandomNumber());
+  }
+}
